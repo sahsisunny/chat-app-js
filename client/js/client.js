165feaa22ca6b2e6c14a98977dc225ca48ff235a
SahsiSunny <sahsisunny@gmail.com>
2023-01-07 07:42:44 +0530

    initial commit
    add all files

---
+++ client/js/client.js
@@ -0,0 +1,45 @@
+const socket = io('http://localhost:8000');
+const messageInput = document.getElementById('message-input');
+const messageContainer = document.querySelector(".container");
+const sendBTN = document.getElementById('send');
+const nameOfUser = document.getElementById('username');
+
+const append = (message, position) => {
+     const messageElement = document.createElement('div');
+     messageElement.innerText = message;
+     messageElement.classList.add('message');
+     messageElement.classList.add(position);
+     messageContainer.append(messageElement);
+}
+const append2 = (message) => {
+     const messageElement = document.createElement('div');
+     messageElement.innerText = message;
+     messageElement.classList.add('user-joined');
+     messageContainer.append(messageElement);
+}
+
+let userName = prompt("Enter your name to join the chat");
+socket.emit('new-user-joined', userName);
+
+socket.on('user-joined', data => {
+     append2(`${data} joined the chat`);
+
+});
+
+sendBTN.addEventListener('click', () => {
+     const message = messageInput.value;
+     append(`You: ${message}`, 'right');
+     socket.emit('send', message);
+     messageInput.value = '';
+     messageContainer.scrollTop = messageContainer.scrollHeight;
+});
+
+socket.on('receive', data => {
+     append(`${data.name}: ${data.message}`, 'left');
+});
+
+socket.on('left', data => {
+     append2(`${data} left the chat`, 'left');
+});
+
+nameOfUser.innerHTML = userName;   
